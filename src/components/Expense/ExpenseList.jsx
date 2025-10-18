b3081ddfc2897091158d54c1a26eca700e7f466b
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-10-18 11:03:20 +0530

    fix the valeue

--- src/components/Expense/ExpenseList.jsx
+++ src/components/Expense/ExpenseList.jsx
@@ -23,7 +23,7 @@ const ExpenseList = ({transactions, onDelete, onDownload}) => {
                         date={moment(expense.date).format("Do MMM YYYY")}
                         amount={expense.amount}
                         type="expense"
-                        onDelete={() => onDelete(income._id)}
+                        onDelete={() => onDelete(expense._id)}
                     />
             ))}
         </div>
