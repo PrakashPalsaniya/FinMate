5a077356f4bbcd17566990338bd46f4f2f2e4e26
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-10-18 11:28:50 +0530

    fsf

--- src/components/Cards/TransactionInfoCard.jsx
+++ src/components/Cards/TransactionInfoCard.jsx
@@ -35,7 +35,7 @@ const TransactionInfoCard = ({ title, icon, date, amount, type, hideDeleteBtn, o
             <div className='flex items-center gap-2'>
                 {!hideDeleteBtn && (
                     <button
-                        className='text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer'
+                        className='text-gray-400 hover:text-red-500 opacity-0 group-hover:opacity-100 transition-opacity cursor-pointer md:opacity-100'
                         onClick={onDelete}
                     >
                         <LuTrash2 size={18} />
