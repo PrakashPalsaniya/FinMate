3fb4ba6d73f7a533babfcd731bc3922ea2b205bc
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-10-18 11:14:02 +0530

    fixedasdf

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,16 +1,18 @@
 import moment from "moment";
 import { LuWalletMinimal, LuLaptop, LuBuilding, LuTrendingUp, LuUtensils, LuHome, LuGamepad2, LuCar, LuZap, LuHeart, LuGraduationCap, LuShoppingBag } from "react-icons/lu";
 
+
+
 export const validEmail = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
     return regex.test(email);
 }
 // /^[^\s@]+@[^\s@]+\.[^\s@]+$/ checks:
 // at least one non-space, non-@ character before @
-// then an @
-// then at least one non-space, non-@ character
-// then a dot .
-// then again at least one non-space, non-@ character
+// t/hen an @
+// /then at least one non-space, non-@ character
+/// then a dot .
+/// then again at least one non-space, non-@ character
 
 export const getInitials = (name) => {
     if (!name) return "";
